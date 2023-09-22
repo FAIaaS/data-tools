6b3c97381742c0cabebac6606255f9d4ac6532de
Oleg Sadov <oleg.sadov@gmail.com>
2023-09-22 13:29:49 +0300

    Minor fixes
    Signed-off-by: Oleg Sadov <oleg.sadov@gmail.com>

--- measure_tools/edge_time_table.sh
+++ measure_tools/edge_time_table.sh
@@ -16,18 +16,18 @@ echo python bin/bandwidth.py $EVE_CSV $BW_CSV \'App. Tx B\' \'App. Rx B\'
 python bin/bandwidth.py $EVE_CSV $BW_CSV 'App. Tx B' 'App. Rx B'
 
 # Clean data
-SMC_CSV=`echo $SM_CSV | sed 's/\.csv/\.cleared.csv/'`
-echo python bin/clean.py $BW_CSV $LM_CSV $SMC_CSV \'App. CPU Load %\' \'App. Tx B/s\' \'App. Rx B/s\'
-python bin/clean.py $BW_CSV $LM_CSV $SMC_CSV 'App. CPU Load %' 'App. Tx B/s' 'App. Rx B/s'
+EVEC_CSV=`echo $EVE_CSV | sed 's/\.csv/\.cleared.csv/'`
+echo python bin/clean.py $BW_CSV $LMC_CSV $EVEC_CSV \'App. CPU Load %\' \'App. Tx B/s\' \'App. Rx B/s\'
+python bin/clean.py $BW_CSV $LMC_CSV $EVEC_CSV 'App. CPU Load %' 'App. Tx B/s' 'App. Rx B/s'
 
-SMS_CSV=`echo $SMC_CSV | sed 's/\.csv/\.smoothed.csv/'`
-echo python bin/smooth.py $SMC_CSV $SMS_CSV \'App. CPU Load %\' \'App. Tx B/s\' \'App. Rx B/s\'
-python bin/smooth.py $SMC_CSV $SMS_CSV 'App. CPU Load %' 'App. Tx B/s' 'App. Rx B/s'
+EVES_CSV=`echo $EVEC_CSV | sed 's/\.csv/\.smoothed.csv/'`
+echo python bin/smooth.py $EVEC_CSV $EVES_CSV \'App. CPU Load %\' \'App. Tx B/s\' \'App. Rx B/s\'
+python bin/smooth.py $EVEC_CSV $EVES_CSV 'App. CPU Load %' 'App. Tx B/s' 'App. Rx B/s'
 
-#echo python bin/time_patterns.py $SMC2_CSV $LMC_CSV
-#python bin/time_patterns.py $SMC2_CSV $LMC_CSV
+#echo python bin/time_patterns.py $EVEC_CSV $LMC_CSV
+#python bin/time_patterns.py $EVEC_CSV $LMC_CSV
 
 TT_CSV="$DOC_DIR/$SRV.time_table.csv"
-echo python bin/time_table.py $SMC_CSV $LMC_CSV $SRV $TT_CSV
-python bin/time_table.py $SMC_CSV $LMC_CSV $SRV $TT_CSV
+echo python bin/time_table.py $EVEC_CSV $LMC_CSV $SRV $TT_CSV
+python bin/time_table.py $EVEC_CSV $LMC_CSV $SRV $TT_CSV
 
